abcc4e09f9731f4051ff10195d18efde78b66ec3
Asmodeus <74126925+ps123sharma@users.noreply.github.com>
2025-04-04 01:25:49 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,5 @@
+API_ID = 10851273  # Your API ID from my.telegram.org
+API_HASH = "b4470eb55a7a6c749f1f178f2acab39f"  # Your API Hash from my.telegram.org
+BOT_TOKEN = "2017379764:AAHekx1U4BZUHjtoSOg14NA0Wac3Br9by_U"  # Your bot token from @BotFather
+SESSION_STRING = "1BVtsOKEBuw8KLY7lkocJ1i3cpAk0oj0vAb3O6LZGzLyatvEmyo4u7tMAMnqoDyNZtypy5TplqUJfZsZIKf1PKV-03VkimubIzhBQ-s2kuHIg_Xtrs9-5MN-r_2N8zO4LCVG_Wp0i2XAPsT5iaBYjKIw458WUutcN1hQ-2iyV-sVIc6RIuGNAyELbVb_mZ21ETeG444UQbRI3uAaxSMLMDRcQHuNoH-oNNkV8vLVBjxTts59VcAv_4K5mcH1la-Xlw9JV88wQVol5ItdfwT9O_74tVGXCRR4cSPs-RN5lNSk8suRMmAUgdy-wr72x2uIrX8vJmDbxPm3fihoxZ8JupQRaoxuYEjw="  # Generate this with generate_session.py
+OWNER_ID = 1194776920  # Your user ID
